d8cbbde81925817f093456637163fc1e5916e491
sanazmahini <sanazmahiniii@gmail.com>
2022-05-18 12:24:44 +0430

    pu

---
+++ src/components/main/ProductCategories.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ProductCategories = () => {
+    return (
+        <>
+            {/* Start Product Categories */}
+            <div className='ProductCategories'>
+            </div>
+            {/* End Product Categories */}
+        </>
+    );
+};
+
+export default ProductCategories;
